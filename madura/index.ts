522a2b18c63c19aee565b6f61156882b6e7fa54c
LukeJ10 <lukas.jiun@gmail.com>
2024-06-28 14:19:05 +0700

    NEW

--- madura/index.ts
+++ madura/index.ts
@@ -1,12 +1,22 @@
 import express, { Request, Response, Application } from "express";
 import { maduraRouter } from "./src/routes/maduraRoute.ts";
-
+import db from "./src/config/database.ts"
 const PORT = 8000;
 const app: Application = express();
 app.use(express.json());
 
 app.use("/madura" , maduraRouter)
 
+db.getConnection((err, connection) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Connected to the database" , connection.threadId);
+  })
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
+
+  
+
